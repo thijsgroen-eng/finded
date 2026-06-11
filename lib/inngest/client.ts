84b510f3651be1185b54c481bb57b548f16a7174
thijsgroen-eng <thijs.groen@zenchef.com>
2026-06-11 12:34:22 +0200

    Create client.ts

---
+++ lib/inngest/client.ts
@@ -0,0 +1,17 @@
+import { Inngest } from 'inngest'
+
+export const inngest = new Inngest({
+  id: 'finded',
+  name: 'Finded AI Visibility',
+})
+
+// Event types
+export type AuditRequestedEvent = {
+  name: 'audit/requested'
+  data: {
+    audit_id: string
+    restaurant_id: string
+  }
+}
+
+export type Events = AuditRequestedEvent
